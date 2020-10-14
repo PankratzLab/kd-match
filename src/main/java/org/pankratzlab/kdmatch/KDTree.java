c59d973928113138b1fb991eade922104a7115b7
jlanej <jlanej@gmail.com>
2020-10-14 13:46:09 -0500

    :floppy_disk:

--- src/main/java/org/pankratzlab/kdmatch/KDTree.java
+++ src/main/java/org/pankratzlab/kdmatch/KDTree.java
@@ -4,33 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//Sourced from https://robowiki.net/wiki/User:Chase-san/Kd-Tree . NB: ZLIB License
+// Sourced from https://robowiki.net/wiki/User:Chase-san/Kd-Tree . NB: ZLIB License
 
-//Copyright (c) <2020> <User:Chase-san>
+// Copyright (c) <2020> <User:Chase-san>
 //
-//This software is provided 'as-is', without any express or implied
-//warranty. In no event will the authors be held liable for any damages
-//arising from the use of this software.
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
 //
-//Permission is granted to anyone to use this software for any purpose,
-//including commercial applications, and to alter it and redistribute it
-//freely, subject to the following restrictions:
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
 //
-//1. The origin of this software must not be misrepresented; you must not
+// 1. The origin of this software must not be misrepresented; you must not
 // claim that you wrote the original software. If you use this software
 // in a product, an acknowledgment in the product documentation would be
 // appreciated but is not required.
-//2. Altered source versions must be plainly marked as such, and must not be
+// 2. Altered source versions must be plainly marked as such, and must not be
 // misrepresented as being the original software.
-//3. This notice may not be removed or altered from any source distribution.
+// 3. This notice may not be removed or altered from any source distribution.
 
-// "Everyone and their brother has one of these now, me and Simonton started it, but I was to
-// inexperienced to get anything written, I took an hour or two to rewrite it today, because I am no
-// longer completely terrible at these things. So here is mine if you care to see it.\n" +
-// "\n" +
-// "This and all my other code in which I display on the robowiki falls under the ZLIB License.\n" +
-// "\n" +
-// "Oh yeah, am I the only one that has a Range function?"
 /**
  * This is a KD Bucket Tree, for fast sorting and searching of K dimensional data.
  * 
